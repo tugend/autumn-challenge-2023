3125983f676b03afd135ab78208fc53d0f0cef82
tugend <tugend@gmail.com>
2023-09-06 22:29:27 +0200

    wip: adding image comparison - works. next todo: finish cleanup

--- Tests/UserInterfacesTests/WebUiTests/StyleTests.cs
+++ Tests/UserInterfacesTests/WebUiTests/StyleTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using Codeuctivity.ImageSharpCompare;
 using FluentAssertions;
 using OpenQA.Selenium;
@@ -7,10 +8,13 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Processing.Processors.Drawing;
+using SixLabors.ImageSharp.Processing.Processors.Filters;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using WebUi;
 using static System.IO.Path;
+using static SixLabors.ImageSharp.PixelFormats.PixelAlphaCompositionMode;
+using static SixLabors.ImageSharp.PixelFormats.PixelColorBlendingMode;
 using Size = System.Drawing.Size;
 
 // https://www.benday.com/2021/07/19/asp-net-core-integration-tests-with-selenium-webapplicationfactory/
@@ -21,11 +25,56 @@ public class StyleTests
     [Fact]
     public async Task SeleniumSanityCheck()
     {
-        var chromeOptions = new ChromeOptions();
-        chromeOptions.AddArgument("--headless");
-        using var driver = new ChromeDriver(chromeOptions);
+        // Startup
+        using var process = await StartWebUi();
+        using var driver = StartChromium();
+        driver.Navigate().GoToUrl("http://localhost:5089/resources/index.html");
 
-        using var process = Process.Start(new ProcessStartInfo
+        // Arrange
+        // Do nothing
+        
+        // Act
+        var screenshot = driver.GetScreenshot();
+        using var actual = Image.Load(screenshot.AsByteArray);
+        
+        // Assert
+        // TODO: custom assertion with side effects
+        // var result = Compare(actual, expected, diffFilePath...)
+        // Assert....
+        var screenshotPath = GetType().Assembly.GetSourceRootPath("UserInterfacesTests", "WebUITests", "StyleTestScreenshots");
+        var expectedPath = Join(screenshotPath, "test.png");
+        using var expected = Image.Load(await File.ReadAllBytesAsync(expectedPath));
+
+        var calculatedDifference = ImageSharpCompare.CalcDiff(actual, expected);
+
+        if (calculatedDifference.PixelErrorPercentage == 0)
+        {
+            true.Should().BeTrue("Benchmark image matches");
+        }
+        
+        using var calcDiff = ImageSharpCompare.CalcDiffMaskImage(actual, expected);
+        var diffPath = Join(screenshotPath, "diff.png");
+        var actualPath = Join(screenshotPath, "actual.png");
+
+        actual.Mutate(x => x.DrawImage(calcDiff, Overlay, DestOver, 0.8f));
+        await actual.SaveAsPngAsync(diffPath);
+        screenshot.SaveAsFile(actualPath, ScreenshotImageFormat.Png);
+
+    }
+
+    private static ChromeDriver StartChromium()
+    {
+        var driverOptions = new ChromeOptions().WithArgument("--headless");
+        var driver = new ChromeDriver(driverOptions);
+        
+        driver.Manage().Window.Size = new Size(900, 900);
+        
+        return driver;
+    }
+
+    private async Task<Process> StartWebUi()
+    {
+        var process = Process.Start(new ProcessStartInfo
         {
             FileName = "cmd.exe",
             WindowStyle = ProcessWindowStyle.Hidden,
@@ -34,31 +83,33 @@ public class StyleTests
             RedirectStandardInput = true,
             RedirectStandardOutput = true
         })!;
-        
-        await process.StandardInput.WriteLineAsync("dotnet run --project ..\\..\\..\\..\\WebUi\\WebUi.csproj");
-        driver.Navigate().GoToUrl("http://localhost:5089/resources/index.html");
-        driver.Manage().Window.Size = new Size(900, 900);
-        
-        var screenshot = driver.GetScreenshot();
-        var assemblyLocation = GetDirectoryName(GetType().Assembly.Location)!;
-        var testFolderPath = Join(assemblyLocation.Split(DirectorySeparatorChar).TakeWhile(x => x != "bin").ToArray());
-        var filePath = Join(testFolderPath, "UserInterfacesTests", "WebUITests", "StyleTestScreenshots", "test.png");
-        var diffPath = Join(testFolderPath, "UserInterfacesTests", "WebUITests", "StyleTestScreenshots", "diff.png");
+        var webUiPath = typeof(Program).Assembly.GetSourceRootPath("WebUi.csproj");
+        await process.StandardInput.WriteLineAsync($"dotnet run --project {webUiPath}");
+        return process;
+    }
+}
 
-        using var actual = Image.Load(screenshot.AsByteArray);
-        using var expected = Image.Load(await File.ReadAllBytesAsync(filePath));
-        using var calcDiff = ImageSharpCompare.CalcDiffMaskImage(actual, expected);
-        await using var fileStreamDifferenceMask = File.Create("differenceMask.png");
-        
-        actual.Mutate(x => x.DrawImage(
-            calcDiff, 
-            PixelColorBlendingMode.Darken, 
-            PixelAlphaCompositionMode.DestAtop, 
-            0.5f));
-        
-        await actual.SaveAsPngAsync(diffPath);
+public static class AssemblyExtensions
+{
+    public static string GetSourceRootPath(this Assembly assembly, params string[] subPath) =>
+        GetDirectoryName(assembly.Location)!
+            .Split(DirectorySeparatorChar)
+            .TakeWhile(x => x != "bin")
+            .Concat(subPath)
+            .PathJoin();
+}
 
-        // screenshot.SaveAsFile(filePath, ScreenshotImageFormat.Png);
+public static class PathExtensions
+{
+    public static string PathJoin(this IEnumerable<string> instance) =>
+        Join(instance.ToArray());
+}
 
+public static class ChromeDriverExtensions 
+{
+    public static ChromeOptions WithArgument(this ChromeOptions instance, string argument)
+    {
+        instance.AddArgument(argument);
+        return instance;
     }
 }
