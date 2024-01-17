b5647d187d1cf1c6f09076e28e355b67ba7fad65
tugend <tugend@gmail.com>
2024-01-17 19:23:45 +0100

    Update visual regressions to newest design (manually verified and compared)

--- WebViewTests/VisualSmokeTests.cs
+++ WebViewTests/VisualSmokeTests.cs
@@ -12,8 +12,10 @@ public sealed class VisualSmokeTests
     public VisualSmokeTests(WebViewTestFixture fixture, ITestOutputHelper outputHelper) => 
         _client = fixture.Inject(outputHelper).Client;
 
-    [Fact]
-    public async Task Shotgun()
+    [Theory]
+    [InlineData("color")]
+    [InlineData("binary")]
+    public async Task Shotgun(string theme)
     {
         // Startup
         var seed = """
@@ -22,7 +24,7 @@ public sealed class VisualSmokeTests
             0 0 0
             """;
 
-        using var _ = _client.StartNewConwaysGame(seed, TimeSpan.FromMilliseconds(800));
+        using var _ = _client.StartNewConwaysGame(seed, TimeSpan.FromMilliseconds(800), theme);
 
         // Act
         for (var i = 1; i < 10; i++)
@@ -31,11 +33,9 @@ public sealed class VisualSmokeTests
             await _client.Benchmark($"turn-{i}");
         }
     }
-    
-    [Theory]
-    [InlineData("color")]
-    [InlineData("binary")]
-    public async Task Interactions_Given_Theme(string theme)
+
+    [Fact]
+    public async Task Interactions()
     {
         var seed = """
             0 2 2
@@ -43,7 +43,7 @@ public sealed class VisualSmokeTests
             2 2 2
             """;
         
-        using var _ = _client.StartNewConwaysGame(seed, TimeSpan.FromMilliseconds(800), theme);
+        using var _ = _client.StartNewConwaysGame(seed, TimeSpan.FromMilliseconds(800), "binary");
 
         var count = 0;
         
